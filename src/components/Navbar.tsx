5cc5e9ae31f4bf8c40eb46822c602183a607e88a
Akite Donald <donaldakite27@gmail.com>
2024-06-05 22:01:30 +0000

    Include Navbar component'

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navbar: React.FC = () => {
+  return (
+    <nav>
+        <img src={"../logo"} alt="" />
+        <ul>
+            <li><a href="/">home</a></li>
+        </ul>
+    </nav>
+  )
+}
+ export default Navbar;
