190cb6fa563a81137cf59c9904901359f1bd3f8a
Akite Donald <donaldakite27@gmail.com>
2024-06-05 22:01:46 +0000

    Add Home page

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+
+
+const HomePage: React.FC = () => {
+    return (
+        <div style={{
+            height: "100vh",
+            background: "blue"
+        }}>
+            <p style={{
+                fontWeight: 700,
+                color: "white"
+            }}>Home Page</p>
+        </div>
+    )
+}
+export default HomePage;
