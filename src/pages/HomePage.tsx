9c5a0f71d5e7efa69933ca201a1edae63c726814
Akite Donald <donaldakite27@gmail.com>
2024-06-06 11:13:26 +0000

    Update sider content array

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -5,22 +5,27 @@ import Slider from '../components/Slider';
 const HomePage: React.FC = () => {
     const slides = [
         {
-          imageUrl: 'https://example.com/image1.jpg',
+          imageUrl: './images/desktop-image-hero-1.jpg',
           altText: 'Image 1',
-          title: 'Slide 1',
-          description: 'Description of slide 1'
+          title: 'Discover innovative ways to decorate',
+          description: 'We provide unmatched quality, comfort, and style for property owners across the country. Our experts combine form and function in bringing your vision to life. Create a room in your own style with our collection and make your property a reflection of you and what you love.'
         },
         {
-          imageUrl: 'https://example.com/image2.jpg',
+          imageUrl: './images/desktop-image-hero-2.jpg',
           altText: 'Image 2',
-          title: 'Slide 2',
-          description: 'Description of slide 2'
+          title: 'We are available all across the globe',
+          description: "With stores all over the world, it's easy for you to find furniture  for your home or place of business. Locally, we're in most major cities throughout the country. Find the branch nearest you using our store locator. Any questions? Don't hesitate to contact us todav."
         },
+        {
+            imageUrl: './images/desktop-image-hero-3.jpg',
+            altText: 'Image 3',
+            title: 'Manufactured with the best materials',
+            description: 'Our modern furniture store provide a high level of quality. Our company has invested in advanced technology to ensure that every product is made as perfect and as consistent as possible. With three decades of experience in this industry, we understand what customers want for their home and office'
+          },
       ];
     return (
         <div style={{
             height: "100vh",
-            background: "blue"
         }}>
             <Slider slides={slides} />
         </div>
